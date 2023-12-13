f901d6de3ae3a366f5ef98c3e92ddd3de4a8c6e9
Ola Imerslund <ola.imerslund@gyldendal.no>
2023-12-13 14:55:07 +0100

    Format files (by saving them)

--- packages/kobber-components/src/utils/progressHelpers.ts
+++ packages/kobber-components/src/utils/progressHelpers.ts
@@ -1,11 +1,8 @@
-export const getProficiencyNameByPercentage = (
-    progressInPercent: number,
-) => {
-    if (progressInPercent === 0)
-        return "unknown";
-    if (progressInPercent < 20) return "low";
-    if (progressInPercent < 40) return "some";
-    if (progressInPercent < 60) return "basics";
-    if (progressInPercent < 80) return "good";
-    return "high";
-}
+export const getProficiencyNameByPercentage = (progressInPercent: number) => {
+  if (progressInPercent === 0) return "unknown";
+  if (progressInPercent < 20) return "low";
+  if (progressInPercent < 40) return "some";
+  if (progressInPercent < 60) return "basics";
+  if (progressInPercent < 80) return "good";
+  return "high";
+};
