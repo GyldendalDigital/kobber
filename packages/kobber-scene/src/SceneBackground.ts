f072ad9e0a8db5f82c6ab647ef7e84868e8136c3
Håvard Kvalvik <havard.kvalvik@gyldendal.no>
2024-01-29 09:35:18 +0100

    Force background repaint

--- packages/kobber-scene/src/SceneBackground.ts
+++ packages/kobber-scene/src/SceneBackground.ts
@@ -24,6 +24,33 @@ export class SceneBackground extends LitElement {
     `,
   ];
 
+  connectedCallback() {
+    this._forceRepaint();
+    super.connectedCallback();
+  }
+
+  disconnectedCallback() {
+    this._stopForcingRepaint();
+    super.disconnectedCallback();
+  }
+
+  // Force Chrome to repaint the background image by updating the layout of the element.
+  // Without this fix Chrome will sometimes not render the background image.
+
+  private _intervalId?: number;
+
+  private _forceRepaint = () => {
+    let padding = 1;
+    this._intervalId = window.setInterval(() => {
+      padding = padding === 1 ? 2 : 1;
+      this.style.setProperty("padding-top", `${padding}px`);
+    }, 1000);
+  };
+
+  private _stopForcingRepaint = () => {
+    clearInterval(this._intervalId);
+  };
+
   render() {
     return html`<slot /> `;
   }
