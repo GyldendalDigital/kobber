458615ba881b81e1a53be7958b1e6ca4ba7139df
Sølve <75790379+solve-selander@users.noreply.github.com>
2024-07-02 16:08:04 +0200

    init commit :)

---
+++ packages/kobber-audio/src/lib/svelte/AudioRecorder.svelte
@@ -0,0 +1,9 @@
+<div>
+    <p>
+        One day, this will be an audio-recorder...
+        For now, here is a circle :)
+    </p>
+    <svg viewBox="0 0 128 128" width="128px">
+        <circle r="64" cx="64" cy="64" fill="chocolate"/>
+    </svg>
+</div>
---
+++ packages/kobber-audio/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import AudioRecorder from "$lib/svelte/AudioRecorder.svelte";
+</script>
+
+<h1>Hello Kobber User!</h1>
+<p>
+    This initial commit is to see that the basic rust component setup is working.
+</p>
+
+<AudioRecorder/>
