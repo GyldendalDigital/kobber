959bcf45def072033f64327db39c539f550c4463
Sølve <75790379+solve-selander@users.noreply.github.com>
2024-07-10 12:59:32 +0200

    typo fix :)

--- packages/kobber-audio/src/routes/+page.svelte
+++ packages/kobber-audio/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <h1>Hello Kobber User!</h1>
 <p>
-    This initial commit is to see that the basic rust component setup is working.
+    This initial commit is to see that the basic svelte component setup is working.
 </p>
 
 <AudioRecorder/>
