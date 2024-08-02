8720afa96ce9810772401bcac3f95d230b39c4fe
Sølve <75790379+solve-selander@users.noreply.github.com>
2024-08-02 12:01:02 +0200

    Removed svelte-kit, went for plain svelte instead

--- packages/kobber-audio/vite.config.js
+++ packages/kobber-audio/vite.config.js
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: "iife",
+        dir: resolve(__dirname, "./dist"),
+        entryFileNames: "main.js",
+      },
+    },
+  },
+})
