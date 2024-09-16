e6b0ed0f98b66218eed4e6325dbfcde88332764e
kevinminh <kevinminh.ng@gmail.com>
2024-09-16 16:26:54 +0200

    Refactor GridLayout component and use KobberGrid from @gyldendal/kobber-components/react

--- apps/wiki/components/grid-layout.tsx
+++ apps/wiki/components/grid-layout.tsx
@@ -10,11 +10,14 @@ type GridLayoutProps = {
 export function GridLayout({ children }: GridLayoutProps) {
   // return <div></div>;
 
+  // With user defined type definition (se kobber.types.tsx)
   // return (
   //   <kobber-grid maxWidth="100%" alignItems="center" gridTemplateColumns={"repeat(1, minmax(0,1fr))"}>
   //     <TopMenu />
   //     {children}
   //   </kobber-grid>
+
+  // Without user defined type definition
   return (
     <KobberGrid maxWidth="100%" alignItems="center" gridTemplateColumns={"repeat(1, minmax(0,1fr))"}>
       <TopMenu />
