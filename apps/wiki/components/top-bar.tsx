27a3d1047367498968b6872dfc75a6aab6669696
kevinminh <kevinminh.ng@gmail.com>
2024-09-16 11:23:24 +0200

    Refactor top-bar component and update tailwind.config.ts
    - Refactor the top-bar component to improve readability and maintainability.
    - Update tailwind.config.ts to add new styles and fix existing styles.

--- apps/wiki/components/top-bar.tsx
+++ apps/wiki/components/top-bar.tsx
@@ -1,35 +1,40 @@
-"use client"
-import { mainRoutes } from "@/config/routes"
-import { cn } from "@/lib/utils"
-import { isOnPath } from "@/utils/is-on-path"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+"use client";
+import { mainRoutes } from "@/config/routes";
+import { cn } from "@/lib/utils";
+import { isOnPath } from "@/utils/is-on-path";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
 
 export function TopBar() {
-	const pathName = usePathname()
+  const pathName = usePathname();
 
-	return (
-		<div className="w-full h-[94px] sticky bg-white/80 backdrop-blur-sm top-0 z-50 ">
-			<div className="mt-[45px]  mx-auto h-[34px] top-[45px]  flex items-center justify-between">
-				<Link href="/" className="text-[32px] font-medium text-[#532D37]">
-					Kobber
-				</Link>
+  return (
+    <div className="w-full h-72 sticky bg-white/80 backdrop-blur-sm top-0 z-50 py-16 px-24">
+      <div className="h-[40px] flex items-center justify-between">
+        <Link href="/" className="text-[20px] font-normal text-[#532D37]">
+          Kobber
+        </Link>
 
-				<ul className="flex text-[#532D37]  items-center gap-[24px]">
-					{mainRoutes.map((route, index) => (
-						<li key={index}>
-							<Link
-								className={cn("text-16 leading-[16px] hover:underline", {
-									underline: isOnPath(pathName, route.href),
-								})}
-								href={route.href}
-							>
-								{route.title}
-							</Link>
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	)
+        <ul className=" text-[#532D37]  items-center gap-[24px] hidden md:flex">
+          {mainRoutes.map((route, index) => (
+            <li key={index}>
+              <Link
+                className={cn("text-16 leading-[16px] hover:underline", {
+                  underline: isOnPath(pathName, route.href),
+                })}
+                href={route.href}
+              >
+                {route.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Button size={"icon"} className="flex md:hidden">
+          <Menu className="size-5" />
+        </Button>
+      </div>
+    </div>
+  );
 }
--- apps/wiki/tailwind.config.ts
+++ apps/wiki/tailwind.config.ts
@@ -1,99 +1,106 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
-	darkMode: ["class"],
-	content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-	prefix: "",
-	theme: {
-		fontSize: {
-			"16": [
-				"16px",
-				{
-					lineHeight: "16px",
-				},
-			],
-		},
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
-		extend: {
-			colors: {
-				"karmin-50": "#fceae9",
-				"karmin-525": "#E50143",
-				"karmin-750": "#75011e",
-				"karmin-850": "#4b0612",
-				"aubergine-25": "#FDF9F9",
-				"aubergine-50": "#f9eaec",
-				"aubergine-250": "#f3a6b2",
-				"aubergine-675": "#8b2241",
-				"aubergine-750": "#6A1830",
-				"aubergine-850": "#481120",
+  darkMode: ["class"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  prefix: "",
+  theme: {
+    fontSize: {
+      "16": [
+        "16px",
+        {
+          lineHeight: "16px",
+        },
+      ],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      height: {
+        "72": "72px",
+      },
+      padding: {
+        "16": "16px",
+        "24": "24px",
+      },
+      colors: {
+        "karmin-50": "#fceae9",
+        "karmin-525": "#E50143",
+        "karmin-750": "#75011e",
+        "karmin-850": "#4b0612",
+        "aubergine-25": "#FDF9F9",
+        "aubergine-50": "#f9eaec",
+        "aubergine-250": "#f3a6b2",
+        "aubergine-675": "#8b2241",
+        "aubergine-750": "#6A1830",
+        "aubergine-850": "#481120",
 
-				"vin-50": "#F3ECED",
-				"vin-150": "#E5CFD3",
-				"vin-325": "#CC94A1",
-				"vin-525": "#A35E70",
-				"vin-600": "#884d5d",
-				border: "hsl(var(--border))",
-				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))",
-				},
-				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
-				},
-			},
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)",
-			},
-			keyframes: {
-				"accordion-down": {
-					from: { height: "0" },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: "0" },
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-			},
-		},
-	},
-	plugins: [require("tailwindcss-animate")],
-} satisfies Config
+        "vin-50": "#F3ECED",
+        "vin-150": "#E5CFD3",
+        "vin-325": "#CC94A1",
+        "vin-525": "#A35E70",
+        "vin-600": "#884d5d",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
 
-export default config
+export default config;
