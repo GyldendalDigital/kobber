4f53073bd43827cf06747b1f2df50157d90b69e8
kevinminh <kevinminh.ng@gmail.com>
2024-09-16 15:30:45 +0200

    Refactor side menu styling and fix link highlighting

--- apps/wiki/components/side-bar-menu/side-menu-item-link.tsx
+++ apps/wiki/components/side-bar-menu/side-menu-item-link.tsx
@@ -1,16 +1,24 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { RouteType } from "@/types/types";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type SideMenuItemLinkProps = {
   route: RouteType;
 };
 
 export function SideMenuItemLink({ route }: SideMenuItemLinkProps) {
+  const pathname = usePathname();
+  const isOnPath = pathname && pathname === route.href;
+
   return (
     <li>
       <Link
         href={route.isComing ? "#" : route.href}
-        className="p-[16px] flex items-center gap-[8px]  h-[48px]  rounded-md hover:bg-red-100/80"
+        className={cn("p-[16px] flex items-center gap-[8px]  h-[48px]  rounded-md hover:bg-aubergine-50", {
+          "underline underline-offset-8 decoration-text/color/action-item/button": isOnPath,
+        })}
       >
         <span className="text-text/color/action-item/button text-sm">{route.title}</span>
 
@@ -19,18 +27,23 @@ export function SideMenuItemLink({ route }: SideMenuItemLinkProps) {
 
       {route.subRoutes && (
         <ul>
-          {route.subRoutes.map((route, index) => (
-            <li className="pl-[40px]" key={index}>
-              <Link
-                href={route.isComing ? "#" : route.href}
-                className="p-[16px] flex items-center gap-[8px]  h-[48px]  rounded-md hover:bg-aubergine-50"
-              >
-                <span className="text-text/color/action-item/button text-sm">{route.title}</span>
+          {route.subRoutes.map((subRoute, index) => {
+            const isSubRouteOnPath = pathname && pathname === subRoute.href;
+            return (
+              <li className="pl-[40px]" key={index}>
+                <Link
+                  href={subRoute.isComing ? "#" : subRoute.href}
+                  className={cn("p-[16px] flex items-center gap-[8px]  h-[48px]  rounded-md hover:bg-aubergine-50", {
+                    "underline underline-offset-8 decoration-text/color/action-item/button": isSubRouteOnPath,
+                  })}
+                >
+                  <span className="text-text/color/action-item/button text-sm">{subRoute.title}</span>
 
-                {route.isComing && <span className="text-[12px] text-text/color/secondary/display-s">kommer</span>}
-              </Link>
-            </li>
-          ))}
+                  {subRoute.isComing && <span className="text-[12px] text-text/color/secondary/display-s">kommer</span>}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       )}
     </li>
--- apps/wiki/components/side-bar-menu/side-menu-item.tsx
+++ apps/wiki/components/side-bar-menu/side-menu-item.tsx
@@ -15,9 +15,9 @@ type SideMenuItemProps = {
 export function SideMenuItem({ routes, name }: SideMenuItemProps) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <Collapsible className="grid gap-[8px] w-[260px] " open={isOpen} onOpenChange={setIsOpen}>
+    <Collapsible className="grid gap-[8px] w-full " open={isOpen} onOpenChange={setIsOpen}>
       <CollapsibleTrigger asChild>
-        <Button className="justify-between mt-2">
+        <Button className="justify-between mt-2 mr-2">
           <span className="uppercase text-text/color/action-item/button">{name}</span>
           {isOpen ? <ChevronUp className="size-6" /> : <ChevronDown className="size-6" />}
         </Button>
