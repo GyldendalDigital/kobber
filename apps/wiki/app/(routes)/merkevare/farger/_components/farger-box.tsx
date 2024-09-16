3d28282ca9b8167e50d586b18bcda51809f919f9
kevinminh <kevinminh.ng@gmail.com>
2024-09-16 15:19:48 +0200

    Refactor layout components and update tailwind.config.ts

---
+++ apps/wiki/app/(routes)/merkevare/farger/_components/farger-box.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export function FargerBox() {
+  return (
+    <div className="w-[270px] bg-zinc-100 h-[220px] rounded-[14px] overflow-hidden p-2 flex items-end justify-start ">
+      <Button>Knapp</Button>
+    </div>
+  );
+}
