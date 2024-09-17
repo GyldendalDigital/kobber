cedd5b6e204c4f6169e61c6e4adb991328edb5aa
kevinminh <kevinminh.ng@gmail.com>
2024-09-17 10:52:44 +0200

    Refactor ColorBlockGridItem component and add copy functionality

--- apps/wiki/components/color-block-grid-item.tsx
+++ apps/wiki/components/color-block-grid-item.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ColorItemType } from "@/types/types";
-import { Copy } from "lucide-react";
+import { Check, Copy } from "lucide-react";
+import { useState } from "react";
 
 type ColorBlockGridItem = {
   color: ColorItemType;
@@ -9,6 +11,21 @@ type ColorBlockGridItem = {
 };
 
 export function ColorBlockGridItem({ color, enableCopy = false }: ColorBlockGridItem) {
+  const [copiedHex, setCopiedHex] = useState<boolean>(false);
+  const [copiedRgb, setCopiedRgb] = useState<boolean>(false);
+
+  const copyToClipboard = (text: string, type: "hex" | "rgb") => {
+    navigator.clipboard.writeText(text).then(() => {
+      if (type === "hex") {
+        setCopiedHex(true);
+        setTimeout(() => setCopiedHex(false), 2000); // Reset after 2 seconds
+      } else {
+        setCopiedRgb(true);
+        setTimeout(() => setCopiedRgb(false), 2000); // Reset after 2 seconds
+      }
+    });
+  };
+
   return (
     <div className="w-fit grid gap-[16px]">
       <div
@@ -22,18 +39,38 @@ export function ColorBlockGridItem({ color, enableCopy = false }: ColorBlockGrid
         <div className="flex items-center gap-4">
           <span className="text-text/color/primary/label-s">HEX {color.hex}</span>
           {enableCopy && (
-            <Button variant={"info"} className="uppercase p-0 h-fit group">
-              Kopier
-              <Copy className="size-4 opacity-0 group-hover:opacity-100 ml-2 transition-opacity" />
+            <Button
+              variant={"info"}
+              className={cn("uppercase p-0 h-fit group", {
+                "opacity-70": copiedHex,
+              })}
+              onClick={() => copyToClipboard(color.hex, "hex")}
+            >
+              Kopier{copiedHex && "t"}
+              {copiedHex ? (
+                <Check className="size-4 text-green-400 ml-2" />
+              ) : (
+                <Copy className="size-4 opacity-0 group-hover:opacity-100 ml-2 transition-opacity" />
+              )}
             </Button>
           )}
         </div>
         <div className="flex items-center gap-4">
           <span className="text-text/color/primary/label-s">RGB {color.rgb}</span>
           {enableCopy && (
-            <Button variant={"info"} className="uppercase p-0 h-fit group">
-              Kopier
-              <Copy className="size-4 opacity-0 group-hover:opacity-100 ml-2 transition-opacity" />
+            <Button
+              variant={"info"}
+              className={cn("uppercase p-0 h-fit group", {
+                "opacity-70": copiedRgb,
+              })}
+              onClick={() => copyToClipboard(color.hex, "rgb")}
+            >
+              Kopier{copiedRgb && "t"}
+              {copiedRgb ? (
+                <Check className="size-4 text-green-400 ml-2" />
+              ) : (
+                <Copy className="size-4 opacity-0 group-hover:opacity-100 ml-2 transition-opacity" />
+              )}
             </Button>
           )}
         </div>
