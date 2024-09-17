28e99e4653b8a03739785b323edd459c692e08ca
Dagfinn Reitan <plagfinn@gmail.com>
2024-09-17 13:39:15 +0200

    remove image domains

--- apps/wiki/next.config.mjs
+++ apps/wiki/next.config.mjs
@@ -3,9 +3,6 @@ import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    domains: ["s3-alpha-sig.figma.com", "res.cloudinary.com"],
-  },
 };
 
 if (process.env.NODE_ENV === "development") {
