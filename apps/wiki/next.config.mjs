4188eabf750c1cafb864da168f5646a440da942d
Dagfinn Reitan <plagfinn@gmail.com>
2024-09-17 14:01:29 +0200

    remove image optimalization

--- apps/wiki/next.config.mjs
+++ apps/wiki/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    // Azure static web apps does not support optimized images
+    // https://learn.microsoft.com/en-us/azure/static-web-apps/deploy-nextjs-hybrid#unsupported-features-in-preview
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
