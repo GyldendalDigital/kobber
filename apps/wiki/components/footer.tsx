b08b63c5a062dfdd5ac58921afe116e96250bdf6
kevinminh <kevinminh.ng@gmail.com>
2024-10-14 14:44:56 +0200

    Refactor footer component layout in wiki app

--- apps/wiki/components/footer.tsx
+++ apps/wiki/components/footer.tsx
@@ -3,7 +3,7 @@ import { LogoSVG } from "./svg";
 export default function Footer() {
   return (
     <div className="w-full  h-[168px] px-main md:px-0 ">
-      <div className="flex items-center justify-between max-w-max-width w-full mx-auto">
+      <div className="flex md:flex-row flex-col items-center md:justify-between max-w-max-width w-full mx-auto gap-16">
         <div className="relative h-32 w-120 ">
           <LogoSVG />
         </div>
