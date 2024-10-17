f27d07d89d928671a8a1930abf55710a6ecd4c4d
kevinminh <kevinminh.ng@gmail.com>
2024-10-17 11:14:42 +0200

    Refactor import paths and update layout in GetStartedLayout component

--- apps/wiki/app/kom-i-gang/layout.tsx
+++ apps/wiki/app/kom-i-gang/layout.tsx
@@ -1,31 +1,27 @@
-import { ContentLayout, ContentShell } from "@/components/content-layout";
-import { SideMenu } from "@/components/menu/side-menu";
-import { SideMenuItemLink } from "@/components/menu/side-menu-item-link";
-import { GetStartedRoutesData } from "./routes-data";
+import { ContentLayout, ContentShell } from "@/components/content-layout"
+import { SideMenu } from "@/components/menu/side-menu"
+import { SideMenuItemLink } from "@/components/menu/side-menu-item-link"
+import { GetStartedRoutesData } from "./routes-data"
 
 type GetStartedLayoutProps = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 export default function GetStartedLayout({ children }: GetStartedLayoutProps) {
   return (
     <ContentLayout>
       <ContentShell>
         <SideMenu>
-          <ul className="grid divide-y divide-wine-150 gap-y-2">
+          <ul className="grid gap-y-2 divide-y divide-wine-150">
             {GetStartedRoutesData.map(({ href, hrefTitle, status }) => (
               <li key={href}>
-                <SideMenuItemLink
-                  href={href}
-                  title={hrefTitle}
-                  status={status}
-                />
+                <SideMenuItemLink href={href} title={hrefTitle} status={status} />
               </li>
             ))}
           </ul>
         </SideMenu>
       </ContentShell>
-      <div className="w-full pb-24 pr-24">{children}</div>
+      <div className="w-full pb-24">{children}</div>
     </ContentLayout>
-  );
+  )
 }
