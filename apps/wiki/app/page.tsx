12f815d6286282468f6a49e4830fe5a2f2d8ffbe
kevinminh <kevinminh.ng@gmail.com>
2024-10-21 11:43:20 +0200

    Refactor import paths and update layout in GetStartedLayout component

--- apps/wiki/app/page.tsx
+++ apps/wiki/app/page.tsx
@@ -1,38 +1,47 @@
-import { Banner } from "@/components/banner";
-import GylImage from "@/public/gyl-art.png";
-import { FeatureBoxType } from "@/types/types";
-import { FeatureBoxGrid } from "@/components/feature-box-grid";
-import { Container } from "@/components/container";
-import Link from "next/link";
-import { ExternalLink } from "lucide-react";
+import Image from "next/image"
+import Link from "next/link"
+import { ExternalLink } from "lucide-react"
+import { FeatureBoxType } from "@/types/types"
+import { FeatureBoxGrid } from "@/components/feature-box-grid"
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-48 pb-20 ">
-      <Banner
-        image={GylImage}
-        className="-mt-72 pt-[175px] md:pt-[150px] h-[calc(700px+72px)] md:h-[calc(900px+72px)]"
-      >
-        <Container />
-      </Banner>
-      <div className="pt-48 md:py-0 pb-0 flex flex-col gap-48 mx-auto max-w-max-width">
+    <main className="mx-auto flex max-w-max-width flex-col gap-48 pb-20">
+      <header className="rounded-[24px] bg-aubergine-50 p-[64px]">
+        <div className="flex items-center justify-between gap-[182px]">
+          <div className="grid gap-[24px]">
+            <div className="flex flex-col gap-[8px] leading-[60px]">
+              <h1 className="text-[48px] text-aubergine-850">Velkommen til Kobber</h1>
+              <h2 className="text-[48px] text-text/color/secondary/display-s">
+                Gyldendals designsystem
+              </h2>
+            </div>
+            <p className="max-w-[40ch] text-[20px] leading-[35px] text-text/color/primary/title-s">
+              Design, bygg og skap gode og helhetlige løsninger med Gyldendals designsystem.
+            </p>
+          </div>
+
+          <Image src={"./hero-svg.svg"} width={351} height={343} alt="Gyldendal Art" />
+        </div>
+      </header>
+      <div className="flex flex-col gap-48 pb-0 pt-48 md:py-0">
         <div className="grid gap-24">
-          <h4 className="text-text/color/primary/heading-s text-primary-heading-s font-normal px-main ">
+          <h4 className="px-main text-primary-heading-s font-normal text-text/color/primary/heading-s">
             Kom i gang
           </h4>
           <FeatureBoxGrid items={boxes} />
         </div>
-        <div className="grid px-main ">
-          <div className="min-h-96 flex flex-col gap-16">
-            <h3 className="text-text/color/primary/title-m text-primary-title-m">
+        <div className="grid px-main">
+          <div className="flex min-h-96 flex-col gap-16">
+            <h3 className="text-primary-title-m text-text/color/primary/title-m">
               Nyttige ressurser
             </h3>
-            <ul className="flex items-center flex-wrap gap-16">
+            <ul className="flex flex-wrap items-center gap-16">
               {links.map((item, index) => (
                 <li key={index}>
                   <Link
                     href={item.title}
-                    className="flex items-center hover:underline gap-8 text-[14px]"
+                    className="flex items-center gap-8 text-[14px] hover:underline"
                   >
                     {item.title}
 
@@ -45,7 +54,7 @@ export default function Home() {
         </div>
       </div>
     </main>
-  );
+  )
 }
 
 const boxes: FeatureBoxType[] = [
@@ -73,7 +82,7 @@ const boxes: FeatureBoxType[] = [
       "https://s3-alpha-sig.figma.com/img/a0e5/4630/c63cadc25209a28c4fd849d7d2656026?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QKWGK~GBCPz6-~TeTUQXc3yViL~dxU86A~K2iP6Nqy0eOVqIC0KtwI7B5NSM~7W1H4STJ4Nd4uZl051ree65HtEUZG3qnqMm78oFM3tHPqw90O4g-90~MFe0WXaRkqaMUHENipdzuIVth~AFaCAly3Pg2u4XA~5TrPWMkLZzHCf6IugR~nY7uzJ04e3dF2Svfkmy5KBWgSuLLvMDKWWU3-CgmyQ6QHjwqZvWmOczFj0zSXcprU~qR6~9eFJ~qOj3271JhMFzcTeA0rs2C8GIcZxj8rZcvoMlauF5FE2cQ13NP00Fg746PwKF3IWwTpkalCZhmZ4pH3JN0BVC4mHnCA__",
     href: "/merkevare/typografi",
   },
-];
+]
 
 const links = [
   {
@@ -92,4 +101,4 @@ const links = [
     title: "Teams",
     href: "#",
   },
-];
+]
