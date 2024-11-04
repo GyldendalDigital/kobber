cad2a03dbb38ba603ec0682fb462dee7996aef11
kevinminh <kevinminh.ng@gmail.com>
2024-11-04 14:55:50 +0100

    Refactor hero-image.tsx to adjust object positioning in HeroImage component

--- apps/wiki/app/(routes)/kom-i-gang/introduksjon/page.tsx
+++ apps/wiki/app/(routes)/kom-i-gang/introduksjon/page.tsx
@@ -12,7 +12,11 @@ export const metadata: PageDetails = {
 export default function Introduksjon() {
   return (
     <SectionLayout>
-      <HeroImage src={placeholderImageUrl({})} />
+      <HeroImage
+        src={
+          "https://dam-p-gyldendal.pqcloud.eu/authkey/1AwlfRxdpz/preview/2bULAP2gabp9rC4A1CbQSB/previews/maxWidth_1600_maxHeight_1600.jpg/*/_DX_2016_preview.jpg?_=1"
+        }
+      />
       <div className="grid gap-section/gap/horizontal">
         <div className="grid gap-y-text-section/gap/header/horizontal">
           <h1 className="text-[48px] font-light leading-[57.6px] text-[#481125]">
@@ -31,22 +35,34 @@ export default function Introduksjon() {
           Kobber er Gyldendals verktøykasse for design- og merkevare. Det er et designsystem
           bestående av gjenbrukbare, fleksible ressurser slik som digitale komponenter, malverk,
           retningslinjer og kode. Samtidig tydeliggjør det vår merkevarestrategi, våre felles
-          verdier og de opplevelsene vi har som mål å tilby våre sluttbrukere. Designsystemet
-          muliggjør en raskere og mer effektiv praksis for konsistent merkevarebygging,
-          produktutvikling og kommunikasjon. Det skaper en tydeligere felles retning, og bidrar til
-          økt kjennskap til og gjenkjennelighet av Gyldendal. Det skal bidra til å styrke
-          fellesskapsfølelsen på tvers av hele Gyldendal, ved å legge til rette for bedre samarbeid,
-          transparens, synergier og deling av kompetanse og metoder på tvers av fagfelt, avdelinger
-          og produkter. Navnet «Kobber» er inspirert av kobberdøra, inngangen til Gyldendalhuset, og
-          symboliserer det tradisjonsrike i kombinasjon med modernisering og fremtid. Kobber er et
-          stabilt og lett bearbeidbart metall som leder strøm, og representerer dermed både våre
-          fysiske produkter og digitale løsninger. Denne nettsiden er en veileder for å sikre
-          konsistent representasjon og implementasjon av Gyldendals merkevare- og designsystem på
-          tvers av kontekster og flater. Den er ment for internt bruk og for eventuelle eksterne
-          samarbeidspartnere. Det er viktig å sette seg inn i merkevaremanualen og designsystemet
-          for å opprettholde merkevarens integritet og styrke over tid. Vi anbefaler å følge
-          retningslinjene, samt komme med tilbakemelding og forslag til oppdatering og forbedring.
-          Sammen skaper vi en levende profil!
+          verdier og de opplevelsene vi har som mål å tilby våre sluttbrukere.
+          <br />
+          <br />
+          Designsystemet muliggjør en raskere og mer effektiv praksis for konsistent
+          merkevarebygging, produktutvikling og kommunikasjon. Det skaper en tydeligere felles
+          retning, og bidrar til økt kjennskap til og gjenkjennelighet av Gyldendal.
+          <br />
+          <br />
+          Det skal bidra til å styrke fellesskapsfølelsen på tvers av hele Gyldendal, ved å legge
+          til rette for bedre samarbeid, transparens, synergier og deling av kompetanse og metoder
+          på tvers av fagfelt, avdelinger og produkter.
+          <br />
+          <br />
+          Navnet «Kobber» er inspirert av kobberdøra, inngangen til Gyldendalhuset, og symboliserer
+          det tradisjonsrike i kombinasjon med modernisering og fremtid. Kobber er et stabilt og
+          lett bearbeidbart metall som leder strøm, og representerer dermed både våre fysiske
+          produkter og digitale løsninger.
+          <br />
+          <br />
+          Denne nettsiden er en veileder for å sikre konsistent representasjon og implementasjon av
+          Gyldendals merkevare- og designsystem på tvers av kontekster og flater. Den er ment for
+          internt bruk og for eventuelle eksterne samarbeidspartnere.
+          <br />
+          <br />
+          Det er viktig å sette seg inn i merkevaremanualen og designsystemet for å opprettholde
+          merkevarens integritet og styrke over tid. Vi anbefaler å følge retningslinjene, samt
+          komme med tilbakemelding og forslag til oppdatering og forbedring. Sammen skaper vi en
+          levende profil!
         </p>
       </div>
     </SectionLayout>
--- apps/wiki/components/footer.tsx
+++ apps/wiki/components/footer.tsx
@@ -1,10 +1,10 @@
-import { LogoSVG } from "./svg"
+import Image from "next/image"
 
 export default function Footer() {
   return (
     <div className="min-h-[206px] w-full">
       <div className="mx-auto flex w-full flex-col items-center pb-[60px] pt-[72px] md:flex-row md:justify-between md:pt-[108px]">
-        <LogoSVG width={131} height={38} />
+        {/* <Image alt="Gyldendal Logo" width={131} height={38} src={"/logo.svg"} /> */}
         <span className="text-center text-[12px] text-[#A35E70]">
           {new Date().getFullYear()} Gyldendal AS
         </span>
