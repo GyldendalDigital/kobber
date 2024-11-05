c3c33772b255f1337e0a0deb24d55823cf22d21a
dsreitan <plagfinn@gmail.com>
2024-11-05 11:05:22 +0100

    Add sentry.io

--- apps/wiki/lib/auth.ts
+++ apps/wiki/lib/auth.ts
@@ -2,6 +2,7 @@ import { db } from "@/drizzle/drizzle"
 import { accounts, users, verificationTokens } from "@/drizzle/schema"
 import MicrosoftEntraID from "@auth/core/providers/microsoft-entra-id"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import * as Sentry from "@sentry/nextjs"
 import NextAuth from "next-auth"
 
 const id = process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER
@@ -12,6 +13,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     usersTable: users,
     verificationTokensTable: verificationTokens,
   }),
+  trustHost: true,
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     MicrosoftEntraID({
@@ -30,4 +32,15 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       },
     }),
   ],
+  logger: {
+    error(code, ...message) {
+      Sentry.captureException(new Error(`ERROR ${code}: ${message ? JSON.stringify(message) : ""}`))
+    },
+    warn(code, ...message) {
+      Sentry.captureMessage(`WARNING ${code}: ${message ? JSON.stringify(message) : ""}`)
+    },
+    debug(code, ...message) {
+      Sentry.captureMessage(`DEBUG ${code}: ${message ? JSON.stringify(message) : ""}`)
+    },
+  },
 })
