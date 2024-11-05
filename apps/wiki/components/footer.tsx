54a6ccdfc4be0201b3bae81579d4eb14ec8b451e
kevinminh <kevinminh.ng@gmail.com>
2024-11-05 11:02:02 +0100

    Refactor footer logo filename to lowercase

--- apps/wiki/components/footer.tsx
+++ apps/wiki/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="min-h-[206px] w-full">
       <div className="mx-auto flex w-full flex-col items-center pb-[60px] pt-[72px] md:flex-row md:justify-between md:pt-[108px]">
-        <Image alt="Gyldendal Logo" width={131} height={38} src={"/Logo.svg"} />
+        <Image alt="Gyldendal Logo" width={131} height={38} src={"/logo.svg"} />
         <span className="text-center text-[12px] text-[#A35E70]">
           © {new Date().getFullYear()} Gyldendal AS
         </span>
