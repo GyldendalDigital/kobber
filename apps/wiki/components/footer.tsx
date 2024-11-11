513322a7cb9efe46a3085c64c2446f131d13506e
kevinminh <kevinminh.ng@gmail.com>
2024-11-11 11:58:05 +0100

    Refactor footer component to add top margin

--- apps/wiki/components/footer.tsx
+++ apps/wiki/components/footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <div className="min-h-[206px] w-full">
+    <div className="mt-auto min-h-[206px] w-full">
       <div className="mx-auto flex w-full flex-col items-center gap-[16px] pb-[60px] pt-[72px] md:flex-row md:justify-between md:pt-[108px]">
         <Image alt="Gyldendal Logo" width={131} height={38} src={"/logo.svg"} />
         <span className="text-center text-[12px] text-[#A35E70]">
