aeababf300d93d48dd77570ea34f5d497ce88e5b
kevinminh <kevinminh.ng@gmail.com>
2024-11-12 15:14:48 +0100

    Added text to pp-mori and lyon

--- apps/wiki/app/(routes)/merkevare/typografi/pp-mori/page.tsx
+++ apps/wiki/app/(routes)/merkevare/typografi/pp-mori/page.tsx
@@ -28,9 +28,15 @@ export default function PPMoriPage() {
           som gjør skriften vennlig og uhøytidelig. Fonten bygger sin identitet rundt bokstaven G og
           sine overdrevne diakritiske tegn, og den kraftige avrundingen i G-versalen spiller godt
           sammen med Gyldendals andre identitetsbærende visuelle grep. F-minuskelen gir et lite hint
-          til det digitale. I vanlig typografisk bruk er det lov å benytte seg av vektene fra Light
-          til Bold, men Regular, Book og Semi Bold er de mest brukte vektene. Til bruk i logoen
-          benyttes variabelvekting.
+          til det digitale.
+          <br />
+          <br />I vanlig typografisk bruk er det lov å benytte seg av vektene fra Light til Bold,
+          men Regular, Book og Semi Bold er de mest brukte vektene. Til bruk i logoen benyttes
+          variabelvekting.
+          <br />
+          <br />
+          Ved bruk av PP Mori kreves det lisens som man må søke tilgang om. Kontakt oss ved behov.
+          Bruk Arial i presentasjoner og dokumenter.
         </p>
       </ArticleWrapper>
       <TypographyList items={PPMoriTypography} />
