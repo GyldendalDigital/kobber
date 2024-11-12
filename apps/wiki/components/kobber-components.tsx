94c5ff8657cf54f1ccea3dd9f5d801eb779d579f
dsreitan <plagfinn@gmail.com>
2024-11-12 14:58:12 +0100

    wiki: load kobber components csr only

--- apps/wiki/components/kobber-components.tsx
+++ apps/wiki/components/kobber-components.tsx
@@ -1,36 +1,34 @@
 "use client"
 
 import {
-  KobberAccordion,
-  KobberArticleWrapper,
-  KobberBody,
-  KobberBoxLayout,
-  KobberButton,
-  KobberCardLayout,
-  KobberCardLayoutColumnAspectRatio,
-  KobberDivider,
-  KobberGrid,
-  KobberGridColumnAspectRatio,
-  KobberIngress,
-  KobberList,
-  KobberListItem,
-} from "@gyldendal/kobber-components/react-ssr-safe"
+  Accordion,
+  ArticleWrapper,
+  Body,
+  BoxLayout,
+  Button,
+  CardLayout,
+  CardLayoutColumnAspectRatio,
+  Divider,
+  Grid,
+  Ingress,
+  List,
+  ListItem,
+} from "./kobber-components-csr" // switch to "./kobber-components-ssr" to test server side rendering
 
 /**
  * Wraps all kobber components in a "use client" file to prevent SSR errors.
  */
 export {
-  KobberButton as Button,
-  KobberBoxLayout as BoxLayout,
-  KobberCardLayout as CardLayout,
-  KobberCardLayoutColumnAspectRatio as CardLayoutColumnAspectRatio,
-  KobberDivider as Divider,
-  KobberAccordion as Accordion,
-  KobberList as List,
-  KobberListItem as ListItem,
-  KobberGrid as Grid,
-  KobberGridColumnAspectRatio as GridColumnAspectRatio,
-  KobberArticleWrapper as ArticleWrapper,
-  KobberBody as Body,
-  KobberIngress as Ingress,
+  Accordion,
+  ArticleWrapper,
+  Body,
+  BoxLayout,
+  Button,
+  CardLayout,
+  CardLayoutColumnAspectRatio,
+  Divider,
+  Grid,
+  Ingress,
+  List,
+  ListItem,
 }
