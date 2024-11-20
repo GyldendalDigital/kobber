0d0a6f8cecbc95fde9d8d10f8407edf72c95b830
kevinminh <kevinminh.ng@gmail.com>
2024-11-20 10:41:20 +0100

    added shared config comments

--- apps/wiki/tailwind.config.ts
+++ apps/wiki/tailwind.config.ts
@@ -1,3 +1,13 @@
+// import sharedConfig from "@gyl"
+// import type { Config } from "tailwindcss"
+
+// const config: Pick<Config, "content" | "presets"> = {
+//   content: ["./src/app/**/*.tsx"],
+//   presets: [sharedConfig],
+// }
+
+// export default config
+
 import type { Config } from "tailwindcss"
 import { tokens } from "./lib/theme"
 
