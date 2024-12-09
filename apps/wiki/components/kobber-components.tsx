dc4c40544d669a6b48c0ebf37860dae56cb8fbda
dsreitan <plagfinn@gmail.com>
2024-12-09 14:46:42 +0100

    wiki: next15 + bun ok

--- apps/wiki/components/kobber-components.tsx
+++ apps/wiki/components/kobber-components.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import {HTMLElement} from '@lit-labs/ssr-dom-shim';
+globalThis.HTMLElement ??= HTMLElement;
+
+
 import {
   Accordion,
-  ArticleWrapper,
+  // ArticleWrapper,
   Body,
   BoxLayout,
   Button,
@@ -14,6 +18,13 @@ import {
   List,
   ListItem,
 } from "./kobber-components-csr" // switch to "./kobber-components-ssr" to test server side rendering
+
+import "@gyldendal/kobber-components/web-components"
+
+// crashes on icons HTMLElement
+const ArticleWrapper = ({ children }) => {
+  return <kobber-article-wrapper>{children}</kobber-article-wrapper>
+}
 
 /**
  * Wraps all kobber components in a "use client" file to prevent SSR errors.
