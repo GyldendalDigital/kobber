fe1c8a615128c07be1fcdd3158b3e29fe6298372
kevinminh <kevinminh.ng@gmail.com>
2024-12-16 10:50:26 +0100

    removed the texthightlightfunction

--- packages/kobber-components/src/button/Button.react.tsx
+++ packages/kobber-components/src/button/Button.react.tsx
@@ -4,21 +4,22 @@ import { HTMLProps } from "react";
 
 // by using variants, we can use <a className={buttonVariants.primary}>
 // Instead of having our own link component
+// We can also use <button className={buttonVariants.primary}>
 const buttonVariants = {
   primary: "insert the classnames here",
   secondary: "insert the classnames here",
   link: "insert the classnames here",
 } as const;
 
-type ButtonProps = Parameters<typeof textHighlightFunctions.classNames>[0] &
-  Pick<HTMLProps<HTMLElement>, "className" | "children">;
-
+type ButtonProps = Pick<HTMLProps<HTMLButtonElement>, "className" | "children"> & {
+  variant: keyof typeof buttonVariants;
+};
 // forwardRef allows passing ref through components
 // allows the parent to access the actual <button> element
 
 // {...props} allows passing through additional props example: disabled={true}
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, children, ...props }, ref) => {
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, children, variant, ...props }, ref) => {
   const buttonClassName = textHighlightFunctions
     .classNames({
       ...props,
@@ -27,7 +28,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, ch
     .join(" ");
 
   return (
-    <button ref={ref} className={buttonClassName} {...props}>
+    <button ref={ref} className={`${buttonVariants[variant]} ${className}`} {...props}>
       {children}
     </button>
   );
