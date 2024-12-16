e941b8c43f94b1e95e740aef123c9609ca0ddcb0
kevinminh <kevinminh.ng@gmail.com>
2024-12-16 10:55:13 +0100

    more comments

--- packages/kobber-components/src/button/Button.react.tsx
+++ packages/kobber-components/src/button/Button.react.tsx
@@ -11,17 +11,21 @@ import {
 // Instead of having our own link component
 // We can also use <button className={buttonVariants.primary}>
 
+// TODO: Add correct variants
 type ButtonVariants = {
   primary: string;
   secondary: string;
   link: string;
 };
+
+// TODO: Add correct classnames / styles
 const buttonVariants: ButtonVariants = {
   primary: "insert the classnames here",
   secondary: "insert the classnames here",
   link: "insert the classnames here",
 } as const;
 
+// TODO: Check for correct props
 type ButtonProps = Pick<HTMLProps<HTMLButtonElement>, "className" | "children"> & {
   variant: keyof ButtonVariants;
   backgroundColor?: ButtonBackgroundColor;
@@ -32,9 +36,7 @@ type ButtonProps = Pick<HTMLProps<HTMLButtonElement>, "className" | "children"> 
 
 // forwardRef allows passing ref through components
 // allows the parent to access the actual <button> element
-
 // {...props} allows passing through additional props example: disabled={true}
-
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, variant, backgroundColor, borderColor, level, iconSettings = "none", ...props }, ref) => {
     return (
@@ -48,4 +50,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 // debugging purposes - easier to find the comp in dev tools
 Button.displayName = "Button";
 
+// Exports both the component and the variants (re-useable variants for example: <a className={buttonVariants.primary}>)
+// import { Button, buttonVariants } from "kobber-components";
 export { Button, buttonVariants };
