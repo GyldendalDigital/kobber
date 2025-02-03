eb6502741c90cdd6c3f17b1545e387ecadbd544c
Andreas Tønseth Myhr <131258582+andreton27@users.noreply.github.com>
2025-02-03 10:28:55 +0100

    Added carousel bug fix

--- packages/kobber-components/src/carousel/Carousel.ts
+++ packages/kobber-components/src/carousel/Carousel.ts
@@ -32,10 +32,10 @@ export class Carousel extends StyledLitElement {
   private _carouselFullWidth = 0;
 
   @state()
-  private _numberOfChildren = 0;
+  private _numberOfChildren: number|undefined=undefined;
 
   @state()
-  private _getTooFewItems = () => this._numberOfChildren < 3;
+  private _getTooFewItems = () => this._numberOfChildren && this._numberOfChildren < 3;
 
   @state()
   private _getItemShrinkValue = () => (this._getTooFewItems() ? -1 : 0);
