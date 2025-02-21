d66a3c661331217835893a90df735fd2c0d5825f
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-02-21 13:22:45 +0100

    Actually use react component styles

--- packages/kobber-components/src/radio/radioInput/RadioInput.react.tsx
+++ packages/kobber-components/src/radio/radioInput/RadioInput.react.tsx
@@ -2,18 +2,22 @@ import { createElement, forwardRef, HTMLProps } from "react";
 import { InputComputedProps, InputProps } from "../Radio.core";
 import "../../button/Button.react";
 import { RadioInputControl } from "../radioInputControl/RadioInputControl.react";
+import { radioInputStyles } from "./RadioInput.styles.react";
 
-type Props = {} & InputProps & HTMLProps<HTMLButtonElement>;
+type Props = InputProps & HTMLProps<HTMLButtonElement>;
 
 export const RadioInput = forwardRef<HTMLButtonElement & HTMLInputElement, Props>((props, ref) => {
   const { checked, disabled, href, variant, children, ...rest } = props;
   const isLink = !!href;
 
   return (
-    <InputTag {...rest} ref={ref} isLink={isLink}>
-      <RadioInputControl checked={checked}></RadioInputControl>
-      {children}
-    </InputTag>
+    <>
+      <style dangerouslySetInnerHTML={{ __html: radioInputStyles.cssText }}></style>
+      <InputTag {...rest} ref={ref} isLink={isLink}>
+        <RadioInputControl checked={checked}></RadioInputControl>
+        {children}
+      </InputTag>
+    </>
   );
 });
 
--- packages/kobber-components/src/radio/radioInputControl/RadioInputControl.react.tsx
+++ packages/kobber-components/src/radio/radioInputControl/RadioInputControl.react.tsx
@@ -5,22 +5,26 @@ import {
   radioInputControlPartName,
   ControlProps,
 } from "../Radio.core";
+import { radioInputControlStyles } from "./RadioInputControl.styles.react";
 
-type Props = {} & ControlProps & HTMLProps<HTMLButtonElement>;
+type Props = ControlProps & HTMLProps<HTMLButtonElement>;
 
 export const RadioInputControl = forwardRef<HTMLDivElement, Props>((props, ref) => {
   const { checked, variant } = props;
 
   return (
-    <div
-      ref={ref}
-      className={[
-        ...controlClassNames({
-          variant,
-        }),
-        radioInputControlPartName,
-        checked ? radioInputControlPartNameChecked : "",
-      ].join(" ")}
-    ></div>
+    <>
+      <style dangerouslySetInnerHTML={{ __html: radioInputControlStyles.cssText }}></style>
+      <div
+        ref={ref}
+        className={[
+          ...controlClassNames({
+            variant,
+          }),
+          radioInputControlPartName,
+          checked ? radioInputControlPartNameChecked : "",
+        ].join(" ")}
+      ></div>
+    </>
   );
 });
