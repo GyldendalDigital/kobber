fd7b05c0a3af23e562bab780ef03b8465111b90f
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-02-21 10:58:40 +0100

    Export component also as web component

--- packages/kobber-components/src/radio/index.tsx
+++ packages/kobber-components/src/radio/index.tsx
@@ -1,3 +1,3 @@
-export { RadioGroup } from "./radioGroup/RadioGroup.react";
-export { RadioInput } from "./radioInput/RadioInput.react";
-export { RadioInputControl } from "./radioInputControl/RadioInputControl.react";
+export { RadioGroup } from "./radioGroup/RadioGroup";
+export { RadioInput } from "./radioInput/RadioInput";
+export { RadioInputControl } from "./radioInputControl/RadioInputControl";
