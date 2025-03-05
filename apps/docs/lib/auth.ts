b5512cd1a95a6849a39ee78ce6c37b295dc88ae2
dsreitan <plagfinn@gmail.com>
2025-03-05 13:59:13 +0100

    docs: remove db

--- apps/docs/lib/auth.ts
+++ apps/docs/lib/auth.ts
@@ -1,26 +1,15 @@
-import { db } from "@/drizzle/drizzle"
-import { accounts, users, verificationTokens } from "@/drizzle/schema"
 import MicrosoftEntraID from "@auth/core/providers/microsoft-entra-id"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import NextAuth from "next-auth"
 
 const id = process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  adapter: DrizzleAdapter(db, {
-    accountsTable: accounts,
-    usersTable: users,
-    verificationTokensTable: verificationTokens,
-  }),
   trustHost: true, // makes the NEXTAUTH_URL env variable obsolete
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
       clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
-
       issuer: process.env.ISSUER_URL,
       authorization: {
         url: `https://login.microsoftonline.com/${id}/oauth2/v2.0/authorize`,
--- apps/docs/types/next-auth.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserType } from "@/drizzle/drizzle"
-
-declare module "next-auth" {
-  interface Session {
-    user: UserType
-  }
-}
