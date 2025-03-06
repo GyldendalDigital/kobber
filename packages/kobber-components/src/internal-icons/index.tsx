fcec5009d6c88277805d3a8e2c157b1755d5701e
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-03-06 16:54:11 +0100

    Correct internal icons usage

---
+++ packages/kobber-components/src/internal-icons/index.tsx
@@ -0,0 +1,3 @@
+export { IconFormChecked as InternalIconFormChecked } from "./form-checked";
+export { InternalIconFormIndeterminate } from "./form-indeterminate";
+export { IconFormRadio as InternalIconFormRadio } from "./form-radio";
