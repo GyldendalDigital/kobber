882dd9af21776b7156409054f7922b852b222634
kevinminhgyl <kevin.minh.nguyen@gyldendal.no>
2025-03-10 14:52:26 +0100

    fix: inject styles for label-react comp

--- packages/kobber-components/src/label/Label.react.tsx
+++ packages/kobber-components/src/label/Label.react.tsx
@@ -1,6 +1,6 @@
 import { ComponentProps, forwardRef } from "react";
-import { labelClassNames, LabelProps } from "./Label.core";
-
+import { labelClassNames, labelName, LabelProps } from "./Label.core";
+import { labelStyles } from "./Label.styles";
 type LabelReactProps = {
   children: string;
   htmlFor?: string; // Important for associating label with form controls
@@ -29,17 +29,20 @@ export const Label = forwardRef<HTMLLabelElement, LabelReactProps>(
     const _label = props["aria-label"];
 
     return (
-      <label
-        ref={ref}
-        {...props}
-        className={[className, ...labelClassNames({ showStatusCircle, theme, variant, size })].join(" ")}
-        aria-label={_label}
-        aria-disabled={ariaDisabled}
-        htmlFor={htmlFor}
-      >
-        {showStatusCircle && <div className="status-circle"></div>}
-        {children}
-      </label>
+      <>
+        <style href={labelName} precedence="medium" dangerouslySetInnerHTML={{ __html: labelStyles.cssText }}></style>
+        <label
+          ref={ref}
+          {...props}
+          className={[className, ...labelClassNames({ showStatusCircle, theme, variant, size })].join(" ")}
+          aria-label={_label}
+          aria-disabled={ariaDisabled}
+          htmlFor={htmlFor}
+        >
+          {showStatusCircle && <div className="status-circle"></div>}
+          {children}
+        </label>
+      </>
     );
   },
 );
