e85ea1010efd88aaec3435342810074067aabd40
kevinminhgyl <kevin.minh.nguyen@gyldendal.no>
2025-03-19 12:49:27 +0100

    feat: badge icon ready for tokens

--- packages/kobber-components/src/badge-icon/BadgeIcon.core.ts
+++ packages/kobber-components/src/badge-icon/BadgeIcon.core.ts
@@ -1,9 +1,42 @@
+import { component } from "@gyldendal/kobber-base/themes/default/tokens.css-variables";
+import { ReplaceSpaceWithDash, replaceSpaceWithDash } from "../utils/replace";
+
 export const badgeIconName = "kobber-badge-icon";
 
-export const badgeIconClassNames = () => {};
+export const badgeIconClassNames = ({
+  theme = "nature",
+  variant = "main",
+  size = "medium",
+}: BadgeIconProps): BadgeIconClassNames[] => {
+  const conditionalClassNames: BadgeIconClassNames[] = [];
+
+  return [badgeIconName, theme, replaceSpaceWithDash(variant), size, ...conditionalClassNames];
+};
 
 export type BadgeIconProps = {
   disabled?: boolean;
+  variant?: BadgeIconVariant;
+  theme?: BadgeIconTheme;
+  size?: BadgeIconSize;
 };
 
-export type BadgeIconClassNames = typeof badgeIconName | "kobber-badge-icon";
+export type BadgeIconClassNames =
+  | typeof badgeIconName
+  | BadgeIconTheme
+  | ReplaceSpaceWithDash<BadgeIconVariant>
+  | BadgeIconSize;
+
+type BadgeIconTheme = "nature" | "fantasy" | "thriller" | "vacation" | "nostalgia" | "Label Info"; // TODO: Wait for tokens
+type BadgeIconVariant = "main" | "supplemental";
+type BadgeIconSize = "medium" | "small";
+
+export const badgeIconThemes: BadgeIconTheme[] = [
+  "nature",
+  "fantasy",
+  "thriller",
+  "vacation",
+  "nostalgia",
+  "Label Info",
+];
+export const badgeIconVariants: BadgeIconVariant[] = ["main", "supplemental"];
+export const badgeIconSizes: BadgeIconSize[] = ["medium", "small"];
