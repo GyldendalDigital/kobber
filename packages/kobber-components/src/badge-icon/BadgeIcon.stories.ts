eb1da2a05ce47ea5f61698a11dd6beee3320038a
kevinminhgyl <kevin.minh.nguyen@gyldendal.no>
2025-03-19 12:49:27 +0100

    feat: badge icon ready for tokens

---
+++ packages/kobber-components/src/badge-icon/BadgeIcon.stories.ts
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import "./BadgeIcon";
+import { badgeIconName, BadgeIconProps, badgeIconSizes, badgeIconThemes, badgeIconVariants } from "./BadgeIcon.core";
+
+interface Args extends BadgeIconProps {
+  text?: string;
+}
+
+const meta: Meta = {
+  title: "Badge Icon",
+  component: badgeIconName,
+  decorators: [
+    (Story, context) => html`<kobber-theme-context theme-id=${context.globals.theme}>${Story()}</kobber-theme-context>`,
+  ],
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+
+export const BadgeIcons: StoryObj<Args> = {
+  argTypes: {
+    size: {
+      options: badgeIconSizes,
+      control: { type: "select" },
+    },
+    theme: {
+      options: badgeIconThemes,
+      control: { type: "select" },
+    },
+    variant: {
+      options: badgeIconVariants,
+      control: { type: "select" },
+    },
+  },
+  args: {
+    text: "Badge Icon",
+    size: "medium",
+    theme: "nature",
+    variant: "main",
+  },
+  render: args => {
+    return html`${renderBadgeIcon(args)}`;
+  },
+};
+
+const renderBadgeIcon = (args: Args) => {
+  const { size, text, theme, variant, disabled } = args;
+
+  return html` <kobber-badge-icon
+    size=${size}
+    theme=${theme}
+    variant=${variant}
+    aria-label="Hello world"
+    disabled=${disabled}
+  >
+    ${text}
+  </kobber-badge-icon>`;
+};
--- packages/kobber-components/src/badge-icon/BadgeIcon.ts
+++ packages/kobber-components/src/badge-icon/BadgeIcon.ts
@@ -3,15 +3,23 @@
  */
 
 import { customElement, property, state } from "lit/decorators.js";
-import { badgeIconName, BadgeIconProps } from "./BadgeIcon.core";
+import { badgeIconClassNames, badgeIconName, BadgeIconProps } from "./BadgeIcon.core";
 import { CSSResultGroup, html, LitElement } from "lit";
 import componentStyles from "../base/styles/component.styles";
 import { badgeIconStyles } from "./BadgeIcon.styles";
-import { badgeClassNames } from "../badge-new/Badge.core";
 
 @customElement(badgeIconName)
 export class BadgeIcon extends LitElement implements BadgeIconProps {
   static styles: CSSResultGroup = [componentStyles, badgeIconStyles];
+
+  @property({ type: String })
+  variant?: BadgeIconProps["variant"] = "main";
+
+  @property({ type: String })
+  theme?: BadgeIconProps["theme"] = "nature";
+
+  @property({ type: String })
+  size?: BadgeIconProps["size"] = "medium";
 
   @property({ type: Boolean })
   disabled?: BadgeIconProps["disabled"];
@@ -19,8 +27,13 @@ export class BadgeIcon extends LitElement implements BadgeIconProps {
   @state()
   private label?: string | null;
 
+  @state()
+  private _hasIcon = false;
+
   connectedCallback() {
     super.connectedCallback();
+
+    this._hasIcon = this.shadowRoot?.host.querySelector("[slot=icon]") !== null;
 
     this.label = this.getAttribute("aria-label");
     this.removeAttribute("aria-label");
@@ -28,11 +41,18 @@ export class BadgeIcon extends LitElement implements BadgeIconProps {
 
   return() {
     return html` <div
-      class="${[...badgeClassNames({}), this.className].join(" ")}"
+      class="${[
+        ...badgeIconClassNames({
+          variant: this.variant,
+          theme: this.theme,
+          size: this.size,
+        }),
+        this.className,
+      ].join(" ")}"
       aria-label=${this.label}
       aria-disabled=${this.disabled ? "true" : "false"}
     >
-      <slot name="badgeIcon"></slot>
+      <slot name="icon"></slot>
       <slot></slot>
     </div>`;
   }
