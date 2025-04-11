a7bef91b1c7756c16b51d8384e96cc16b2f5f424
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-04-11 13:42:35 +0200

    WIP: Build svg react components as react
    - Use svg-jsx-converter package
    
    TODO:
    1. Move files to react assets folder, and rename to correct case.
    2. Move built files to each component folder.
    - Consider using imports for icon types.
    - Consider referring to component, instead of copying icon components.

---
+++ packages/kobber-icons/sjc.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  src: "./src/assets/svgs-react",
+  dest: "./src/icon/icons-react",
+  type: "tsx",
+  imports: [],
+  memo: false,
+  fcType: "default",
+  component: true,
+  cleanupIDs: true,
+};
