103ba6e9cb0ae3f61e24f29823629aadbc7fd3e5
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-04-30 14:46:40 +0200

    Remove text story summaries
    Use storybook's autoDocs feature instead.
    
    story-summary was added in 1f52970.

--- packages/kobber-components/src/link/Link.stories.ts
+++ packages/kobber-components/src/link/Link.stories.ts
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
-import { storySummary } from "../story/story-summary";
-import { linkStyles } from "./Link.styles";
 import "./Link";
 import "@gyldendal/kobber-icons/web-components";
 import { LinkProps, linkTypes } from "./Link.core";
@@ -25,6 +23,7 @@ const meta: Meta<Args> = {
       control: { type: "radio" },
     },
   },
+  tags: ["autodocs"],
   decorators: [(Story, context) => html`<div class="${context.globals.theme}">${Story()}</div>`],
   parameters: {
     layout: "centered",
@@ -35,6 +34,9 @@ export default meta;
 
 const linkStates = ["idle", "active", "hover", "focus", "disabled"];
 
+/**
+ * Disabled er ikke en gyldig state for lenker. Disabled fjerner href-attributten.
+ */
 export const Link: StoryObj<Args> = {
   args: {
     text: "",
@@ -43,43 +45,38 @@ export const Link: StoryObj<Args> = {
   },
   render: args => {
     return html`<div style="max-width: 600px;">
-        <kobber-text-wrapper>
-          ${linkStates.map(state => {
-            return html`<p>
-              <kobber-link
-                class="${state}"
-                href=${state !== "disabled" ? "https://github.com/GyldendalDigital/kobber" : undefined}
-                type="${args.type}"
-                ${args.icon === "left" ? "iconFirst" : ""}
-              >
-                ${args.text || `Lenke med tilstand ${state}`}
-                ${args.icon !== "none" ? html`<kobber-arrow_right slot="icon" />` : ""}
-              </kobber-link>
-            </p>`;
-          })}
-        </kobber-text-wrapper>
-        <br />
-        <br />
-        <kobber-text-wrapper>
-          ${linkStates.map(state => {
-            return html`<p>
-              <kobber-link
-                class="${state}"
-                onClick="alert('Hello world!')"
-                type="${args.type}"
-                ${args.icon === "left" ? "iconFirst" : ""}
-              >
-                ${args.text || `Knapp med tilstand ${state}`}
-                ${args.icon !== "none" ? html`<kobber-arrow_right slot="icon" />` : ""}
-              </kobber-link>
-            </p>`;
-          })}
-        </kobber-text-wrapper>
-      </div>
-
-      ${storySummary({
-        summary: `Disabled er ikke en gyldig state for lenker. Disabled fjerner href-attributten.`,
-        code: linkStyles.cssText,
-      })}`;
+      <kobber-text-wrapper>
+        ${linkStates.map(state => {
+          return html`<p>
+            <kobber-link
+              class="${state}"
+              href=${state !== "disabled" ? "https://github.com/GyldendalDigital/kobber" : undefined}
+              type="${args.type}"
+              ${args.icon === "left" ? "iconFirst" : ""}
+            >
+              ${args.text || `Lenke med tilstand ${state}`}
+              ${args.icon !== "none" ? html`<kobber-arrow_right slot="icon" />` : ""}
+            </kobber-link>
+          </p>`;
+        })}
+      </kobber-text-wrapper>
+      <br />
+      <br />
+      <kobber-text-wrapper>
+        ${linkStates.map(state => {
+          return html`<p>
+            <kobber-link
+              class="${state}"
+              onClick="alert('Hello world!')"
+              type="${args.type}"
+              ${args.icon === "left" ? "iconFirst" : ""}
+            >
+              ${args.text || `Knapp med tilstand ${state}`}
+              ${args.icon !== "none" ? html`<kobber-arrow_right slot="icon" />` : ""}
+            </kobber-link>
+          </p>`;
+        })}
+      </kobber-text-wrapper>
+    </div>`;
   },
 };
