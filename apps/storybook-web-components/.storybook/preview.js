e91f5d399d542163ec77da1a09c01dca0883e632
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-05-26 14:15:42 +0200

    Enable icons in storybook components
    By importing icons in .storybook/preview.js.

--- apps/storybook-web-components/.storybook/preview.js
+++ apps/storybook-web-components/.storybook/preview.js
@@ -1,5 +1,6 @@
 import "@gyldendal/kobber-base/themes/default/tokens.css";
 import "@gyldendal/kobber-base/themes/dark/tokens.css";
+import "@gyldendal/kobber-icons/web-components";
 
 const themes = ["kobber-theme-default", "kobber-theme-dark"];
 
