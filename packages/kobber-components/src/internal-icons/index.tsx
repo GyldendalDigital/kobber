8e6056b68d55ccea867795da34f485cfbea735bd
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-05-26 14:14:38 +0200

    Correct and simplify internal form icons

--- packages/kobber-components/src/internal-icons/index.tsx
+++ packages/kobber-components/src/internal-icons/index.tsx
@@ -1,3 +1,3 @@
-export { IconFormChecked } from "./form-checked";
-export { IconFormIndeterminate } from "./form-indeterminate";
-export { IconFormRadio } from "./form-radio";
+export { IconFormChecked, iconFormCheckedName } from "./form-checked";
+export { IconFormIndeterminate, iconFormIndeterminateName } from "./form-indeterminate";
+export { IconFormRadio, iconFormRadioName } from "./form-radio";
