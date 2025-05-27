eb96d908d8497cd52d5279522265b2e854e9ecc3
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-05-27 15:21:39 +0200

    Remove extra index file for internal export of components

--- packages/kobber-components/src/radio/index.ts
+++
@@ -1,3 +0,0 @@
-export { RadioGroup } from "./radio-group/RadioGroup";
-export { RadioInput } from "./radio-input/RadioInput";
-export { RadioInputControl } from "./radio-input-control/RadioInputControl";
