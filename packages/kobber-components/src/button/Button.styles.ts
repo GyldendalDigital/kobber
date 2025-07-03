5ff251e060406c040debc684261b3292c54deb41
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-07-03 14:19:49 +0200

    Use css values from tokens

--- packages/kobber-components/src/button/Button.styles.ts
+++ packages/kobber-components/src/button/Button.styles.ts
@@ -4,15 +4,11 @@ import { component, universal, typography } from "@gyldendal/kobber-base/themes/
 import { ButtonClassNames, buttonDefaultProps, ButtonProps, buttonThemeProps, buttonUiProps } from "./Button.core";
 import { resetButton } from "../base/styles/reset.styles";
 
-// TODO: get from tokens
-const paddingIconOnly = 12 / 16 + "rem";
-
 /**
  * Shared styles, used in web component, React and CSS module.
  *
  * TODO:
  * - choose between padding block or fixed height. Both are not needed.
- * - padding as icon only should be a token, or use fixed width same as height
  */
 const createButtonStyles = () => {
   const button = component.button;
@@ -55,9 +51,7 @@ const createButtonStyles = () => {
 
       &[disabled],
       &.disabled {
-        /* TODO: wait for tokens to expose percent as number, not rem */
-        /* opacity: var(${unsafeCSS(universal.disabled.container.opacity)}); */
-        opacity: 0.5;
+        opacity: var(${unsafeCSS(universal.disabled.container.opacity)});
         cursor: auto;
       }
 
@@ -170,8 +164,8 @@ const hoverEffectSecondary = () => css`
       &.${unsafeCSS("kobber-button--icon" satisfies ButtonClassNames)} {
         &.${unsafeCSS("kobber-button--icon-only" satisfies ButtonClassNames)} {
           &:after {
-            right: ${unsafeCSS(paddingIconOnly)};
-            left: ${unsafeCSS(paddingIconOnly)};
+            right: ${unsafeCSS(component.button.container.padding.block["icon-only"])};
+            left: ${unsafeCSS(component.button.container.padding.block["icon-only"])};
           }
         }
       }
