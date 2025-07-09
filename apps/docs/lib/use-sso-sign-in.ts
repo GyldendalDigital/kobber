85c3b5349cb8c5f9dc33156feb1e6261dbfc00b2
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-07-09 15:15:28 +0200

    Remove ts errors after storybook upgrade
    Main changes:
    - Upgrade turbo package
    - Add type definitions for package babel-generator

--- apps/docs/lib/use-sso-sign-in.ts
+++ apps/docs/lib/use-sso-sign-in.ts
@@ -1,8 +1,8 @@
-import { signIn } from "next-auth/react"
+import { signIn, SignInOptions } from "next-auth/react"
 
 export const ssoSignIn = ({ redirectUrl }: { redirectUrl?: string }) => {
   signIn("microsoft-entra-id", {
     redirect: redirectUrl ? true : false,
     redirectTo: redirectUrl,
-  })
+  } as SignInOptions)
 }
