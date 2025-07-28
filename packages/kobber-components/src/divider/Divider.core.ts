ca74c7c7bc40533b20af45ffecae92371e4ad58b
Ola Imerslund <ola.imerslund@gyldendal.no>
2025-07-28 14:42:11 +0200

    Clean up divider styles
    - move styling from Divider.ts to Divider.styles.ts
    - generate type-safe styles for variants

--- packages/kobber-components/src/divider/Divider.core.ts
+++ packages/kobber-components/src/divider/Divider.core.ts
@@ -1,1 +1,14 @@
-export type DividerVariant = "main" | "supplemental";
+import { component } from "@gyldendal/kobber-base/themes/tokens.css-variables.js";
+
+const dividerTokens = component.divider;
+
+export const dividerName = "kobber-divider";
+
+export type DividerClassNames = typeof dividerName;
+export type DividerVariant = keyof typeof dividerTokens.background.color;
+
+export const dividerClassnames = (): DividerClassNames[] => {
+  return [dividerName];
+};
+
+export const dividerVariants = Object.keys(dividerTokens.background.color) as DividerVariant[];
