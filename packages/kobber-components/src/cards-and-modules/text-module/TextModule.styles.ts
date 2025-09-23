67d5c5f2be84af6f1360640be61ae4932ea1be05
olabaloo <ola.imerslund@gyldendal.no>
2025-09-23 12:31:20 +0200

    Add TextModule and TextBlock components

---
+++ packages/kobber-components/src/cards-and-modules/text-module/TextModule.styles.ts
@@ -0,0 +1,59 @@
+import { css, unsafeCSS } from "lit";
+
+import {
+  type TextModuleClassNames,
+  textModuleTokens,
+  textModuleColors,
+  textModuleColorVariants,
+} from "./TextModule.core";
+
+const createTextModuleStyles = () => {
+  return css`
+    :host {
+      display: block;
+    }
+    .${unsafeCSS("kobber-text-module" satisfies TextModuleClassNames)} {
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      gap: var(${unsafeCSS(textModuleTokens["inner-text-container"].gap)});
+      padding: var(${unsafeCSS(textModuleTokens.padding.top)}) var(${unsafeCSS(textModuleTokens.padding.inline)}) var(${unsafeCSS(textModuleTokens.padding.bottom)});
+      min-height: 5em;
+      border-radius: 1.5em;
+      
+      max-width: 600px;
+      ::slotted(kobber-text-module) {
+        max-width: 400px;
+      }
+
+      .header, .div {
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        gap: var(${unsafeCSS(textModuleTokens.gap)});
+      }
+
+      background-color: var(--text-module-background-color);
+      ${colorVariants()};
+    }
+  `;
+};
+
+const colorVariants = () =>
+  unsafeCSS(
+    textModuleColors
+      .flatMap(textModuleColor => {
+        return textModuleColorVariants.flatMap(textModuleColorVariant => {
+          return `
+      &[data-color="${textModuleColor}"] {
+          &[data-color-variant="${textModuleColorVariant}"] {
+            --text-module-background-color: var(${textModuleTokens.color.background[textModuleColor][textModuleColorVariant]});
+          }
+        }`;
+        });
+      })
+      .join("\n"),
+  );
+
+export const textModuleStyles = createTextModuleStyles();
