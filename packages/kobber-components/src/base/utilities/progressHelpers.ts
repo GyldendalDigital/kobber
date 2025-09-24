50a172dbbab2cb8d26c7a9488f01ff0e76061e44
olabaloo <ola.imerslund@gyldendal.no>
2025-09-24 15:28:58 +0200

    Remove ProgressBar component

--- packages/kobber-components/src/base/utilities/progressHelpers.ts
+++
@@ -1,8 +0,0 @@
-export const getProficiencyNameByPercentage = (progressInPercent: number) => {
-  if (progressInPercent === 0) return "unknown";
-  if (progressInPercent < 20) return "low";
-  if (progressInPercent < 40) return "some";
-  if (progressInPercent < 60) return "basics";
-  if (progressInPercent < 80) return "good";
-  return "high";
-};
