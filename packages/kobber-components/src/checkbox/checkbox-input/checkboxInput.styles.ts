0241be701522de7cb590ed323a2bebd89db876b2
olabaloo <ola.imerslund@gyldendal.no>
2025-10-01 09:57:26 +0200

    Add color to checkbox indicator icon

--- packages/kobber-components/src/checkbox/checkbox-input/checkboxInput.styles.ts
+++ packages/kobber-components/src/checkbox/checkbox-input/checkboxInput.styles.ts
@@ -64,6 +64,7 @@ const createCheckboxStyles = () => {
       border-radius: var(${unsafeCSS(checkbox.outline.border.radius)});
       border: var(${unsafeCSS(indicator.border.width)}) solid var(--control-border-color);
       outline: var(${unsafeCSS(checkbox.outline.border.width)}) solid var(--control-outline-color);
+      color: var(--color)
       flex-shrink: 0;
       background-color: var(--control-background-color);
       transition: var(--transition-time) outline;
@@ -84,6 +85,7 @@ const colorThemeStyles = () => {
   const colorThemes = checkboxColorThemes.flatMap(colorTheme => {
     return css`
       ${unsafeCSS(`&[data-color="${colorTheme}"]`)} {
+        --color: var(${unsafeCSS(indicator.shape.color[colorTheme])});
         ${statesPerColorTheme(colorTheme)}
       }
     `;
