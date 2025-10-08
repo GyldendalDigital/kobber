127fc81c6fba133a614e6b083f800b219418c748
olabaloo <ola.imerslund@gyldendal.no>
2025-10-08 06:56:16 +0200

    Add note on list style types

--- packages/kobber-components/src/text/text-list/TextList.core.ts
+++ packages/kobber-components/src/text/text-list/TextList.core.ts
@@ -29,5 +29,11 @@ export const textListTypes = ["ordered", "unordered"] as const;
 export const textListTypeFallback: TextListType = "ordered";
 export const textListSizeFallback: TextListSize = "medium";
 
+/**
+ * List style types: These must either be found among predefined counter
+ * styles (https://w3c.github.io/predefined-counter-styles/),
+ * or must be defined as our own @counter-style
+ * (https://www.w3.org/TR/css-counter-styles-3/).
+ */
 export const textListOrderedStyleTypes = ["decimal", "lower-alpha"];
 export const textListUnorderedStyleTypes = ["disc"];
