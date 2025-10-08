a1a29fd43ca203e7cff2c9413a1c75d254eb6755
olabaloo <ola.imerslund@gyldendal.no>
2025-10-08 11:50:43 +0200

    Rename cards-and-modules to content-blocks
    As in Figma.

---
+++ packages/kobber-components/src/content-blocks/text-module/TextModule.stories.ts
@@ -0,0 +1,85 @@
+import type { Args, Meta, StoryObj } from "@storybook/web-components-vite";
+import { textModuleColors, textModuleColorVariants } from "./TextModule.core";
+import "./TextModule";
+import "../components/text-block/TextBlock";
+import "../../badge-icon/BadgeIcon";
+import "../../text/heading/Heading";
+import "../../text/title/Title";
+import "../../text/text-body/TextBody";
+import { html } from "lit";
+import "../../theme-context-provider/ThemeContext";
+import "@gyldendal/kobber-icons/web-components";
+import { init as initComponents } from "../../base/init";
+import { init as initIcons } from "@gyldendal/kobber-icons/init";
+import { invertColorVariant } from "../../base/utilities/invertColorVariant";
+import { nestedTextModule } from "../../story/nestedTextModule";
+
+initComponents();
+initIcons();
+
+const meta: Meta = {
+  title: "Content Blocks/Text Module",
+  argTypes: {
+    color: {
+      options: textModuleColors,
+      control: { type: "inline-radio" },
+    },
+  },
+  args: {
+    color: textModuleColors[0],
+    showBadge: true,
+    showHeading: true,
+    showNested: false,
+  },
+};
+
+export default meta;
+type Story = StoryObj;
+
+const mappedColor = (args: Args) => {
+  if (args.color === "transparent") {
+    return "neutral";
+  }
+  return args.color;
+};
+
+export const TextModule: Story = {
+  argTypes: {
+    colorVariant: {
+      options: textModuleColorVariants,
+      control: { type: "inline-radio" },
+    },
+  },
+  args: {
+    colorVariant: textModuleColorVariants[0],
+  },
+  render: args => html`
+    <kobber-text-module color="${args.color}" color-variant="${args.colorVariant}">
+    ${
+      !args.showBadge
+        ? ""
+        : html`
+      <kobber-badge-icon slot="badge" color-theme="${mappedColor(args)}" color-variant="${invertColorVariant(args.colorVariant)}">
+        <kobber-pencil slot="icon"></kobber-pencil>
+        Badge text
+      </kobber-badge-icon>`
+    }
+
+      ${
+        !args.showHeading
+          ? ""
+          : html`<kobber-heading slot="heading" color="${mappedColor(args)}" color-variant="${invertColorVariant(args.colorVariant)}">Heading</kobber-heading>`
+      }
+
+      <kobber-text-block>
+        <kobber-title slot="title" color="${mappedColor(args)}" color-variant="${invertColorVariant(args.colorVariant)}" size="large">Title L</kobber-title>
+        <kobber-text-body color="${mappedColor(args)}" color-variant="${invertColorVariant(args.colorVariant)}">Body text here. Lorem ipsum dolor sit amet, consectetur adipiscing el it. Ut et massa mi. (${args.color})</kobber-text-body>
+      </kobber-text-block>
+      ${args.showNested ? nestedTextModule(args) : ""}
+      <kobber-text-block>
+        <kobber-title slot="title" color="${mappedColor(args)}" color-variant="${invertColorVariant(args.colorVariant)}" size="medium">Title M</kobber-title>
+        <kobber-text-body color="${mappedColor(args)}" color-variant="${invertColorVariant(args.colorVariant)}">Body text here. (${args.color})</kobber-text-body>
+      </kobber-text-block>
+    </kobber-text-module>
+  `,
+};
