376c9fb3e573f3f04e68c0a9aa0c6d988684af32
Håvard Kvalvik <havard.kvalvik@gyldendal.no>
2025-11-05 11:48:47 +0100

    Preview cleanup

--- packages/kobber-components-poc/src/preview/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Select } from "../components/react/select/Select";
-
-export function App() {
-  return (
-    <div>
-      <Select disabled />
-    </div>
-  );
-}
--- packages/kobber-components-poc/src/preview/index.ts
+++ packages/kobber-components-poc/src/preview/index.ts
@@ -1,18 +1,16 @@
-// src/main.js
 import { createElement } from "react";
 import ReactDOM from "react-dom/client";
 import { mount } from "svelte";
-//import type { App } from "./App";
-import { AppReact } from "./AppReact"; // Your React main component
-import AppSvelte from "./AppSvelte.svelte"; // Your Svelte main component
+import { AppReact } from "./AppReact";
+import AppSvelte from "./AppSvelte.svelte";
 import "./cssImports";
+
+console.log("###");
 
 const reactContainer = document.getElementById("react");
 if (!reactContainer) throw new Error("Could not find react container");
 
 ReactDOM.createRoot(reactContainer).render(createElement(AppReact));
-
-// Mount Svelte app
 
 const svelteContainer = document.getElementById("svelte");
 if (!svelteContainer) throw new Error("Could not find svelte container");
