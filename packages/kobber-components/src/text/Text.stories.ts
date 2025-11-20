aa82d20c7f561c3c5bc87c50dfec8f5ef12b8161
olabaloo <ola.imerslund@gyldendal.no>
2025-11-20 14:48:24 +0100

    Correct TextListElement color and colorVariant uses

--- packages/kobber-components/src/text/Text.stories.ts
+++ packages/kobber-components/src/text/Text.stories.ts
@@ -472,6 +472,10 @@ const ifHighlighted = (textValue: string, highlighted: boolean) =>
  */
 export const TextList: Story = {
   argTypes: {
+    color: {
+      options: textBodyColors,
+      control: "inline-radio",
+    },
     colorVariant: {
       options: textBodyColorVariants,
       control: "inline-radio",
@@ -482,38 +486,45 @@ export const TextList: Story = {
     },
   },
   args: {
+    color: textBodyColors[0],
     colorVariant: textBodyColorVariants[0],
-    size: textBodySizes[1],
+    size: textListSizeFallback,
   },
   render: (args: Args) => {
     return html`
       <kobber-text-list size="${args.size}"
+        color="${args.color}"
         color-variant="${args.colorVariant}"
       >
-        <kobber-text-list-element>
+        <kobber-text-list-element
+        color="${args.color}"
+        color-variant="${args.colorVariant}">
           Punkt
           <kobber-text-list slot="nested" size="${args.size}">
-            <kobber-text-list-element>
+            <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
               Underpunkt
             </kobber-text-list-element>
-            <kobber-text-list-element>
+            <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
               Underpunkt
             </kobber-text-list-element>
-            <kobber-text-list-element>
+            <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
+              Underpunkt
+            </kobber-text-list-element>
+            <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
               Underpunkt
             </kobber-text-list-element>
           </kobber-text-list>
         </kobber-text-list-element>
-        <kobber-text-list-element>
+        <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
           Punkt
           <kobber-text-list slot="nested" size="${args.size}">
-            <kobber-text-list-element>
+            <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
               Underpunkt
             </kobber-text-list-element>
-            <kobber-text-list-element>
+            <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
               Underpunkt
             </kobber-text-list-element>
-            <kobber-text-list-element>
+            <kobber-text-list-element color="${args.color}" color-variant="${args.colorVariant}">
               Underpunkt
             </kobber-text-list-element>
           </kobber-text-list>
--- packages/kobber-components/src/text/text-list-element/TextListElement.ts
+++ packages/kobber-components/src/text/text-list-element/TextListElement.ts
@@ -3,14 +3,25 @@ import { LitElement } from "lit";
 import { html } from "lit/static-html.js";
 import componentStyles from "../../base/styles/component.styles";
 import { customElement } from "../../base/utilities/customElementDecorator";
-import { nestedListSlotName, textListElementName } from "./TextListElement.core";
+import {
+  nestedListSlotName,
+  type TextListElementProps,
+  textListElementName,
+} from "./TextListElement.core";
 import { textListElementStyles } from "./TextListElement.styles";
 import "../text-body/TextBody";
-import { state } from "lit/decorators.js";
+import { property, state } from "lit/decorators.js";
+import { ifDefined } from "lit/directives/if-defined.js";
 
 @customElement(textListElementName)
 export class TextListElement extends LitElement {
   static styles: CSSResultGroup = [componentStyles, textListElementStyles];
+
+  @property({ attribute: "color" })
+  color: TextListElementProps["color"] = "brand";
+
+  @property({ attribute: "color-variant" })
+  colorVariant: TextListElementProps["colorVariant"] = "tone-a";
 
   @state()
   protected _hasNestedList = false;
@@ -26,7 +37,7 @@ export class TextListElement extends LitElement {
       <div role="listitem" class="${textListElementName}"
         data-has-nested-list="${this._hasNestedList}"
       >
-        <kobber-text-body class="body">
+        <kobber-text-body class="body" color="${ifDefined(this.color)}" color-variant="${ifDefined(this.colorVariant)}">
           <slot></slot>
           <slot name="${nestedListSlotName}"></slot>
         </kobber-text-body>
